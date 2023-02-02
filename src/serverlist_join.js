fdf54d1cd9c9278f531dee5c624fa19b4e6a452c
ericek111 <erik.brocko@letemsvetemapplem.eu>
2023-02-02 17:11:08 +0100

    Connect buttons in server list

---
+++ src/serverlist_join.js
@@ -0,0 +1,38 @@
+(() => {
+	const ipRows = document.querySelectorAll('article.content-server > div.server > .ip');
+	if (ipRows.length === 0) {
+		return;
+	}
+
+	const serverListStyle = document.createElement('STYLE');
+	serverListStyle.innerText = `
+		article div.server {
+			display: flex;
+		}
+		.ft-join-server-button a {
+			color: #238ad4;
+			font-weight: bold;
+			padding: 0.75em 1em;
+		}
+		article div.server div.ip {
+			min-width: 135px;
+			width: auto;
+		}
+		article div.server div.players {
+			text-align: center;
+		}
+	`;
+	document.head.appendChild(serverListStyle);
+
+	const joinButtonTempl = document.createElement('DIV');
+	joinButtonTempl.classList.add('ft-join-server-button');
+	const joinLinkTempl = document.createElement('A');
+	joinLinkTempl.innerText = 'Connect';
+	joinButtonTempl.appendChild(joinLinkTempl);
+
+	for (const ipRow of ipRows) {
+		const joinServerDiv = joinButtonTempl.cloneNode(true);
+		joinButtonTempl.firstChild.href = 'steam://connect/' + ipRow.innerText.trim();
+		ipRow.insertAdjacentElement('afterend', joinServerDiv);
+	}
+})();
